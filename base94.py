368016e8a4aa87a9869ea36218170f4f5a673d05
Oleksii Tsvietnov <vorakl@protonmail.com>
2020-04-18 23:42:04 -0500

    Add comments

--- base94.py
+++ base94.py
@@ -9,27 +9,33 @@ abc = '''!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefg
 def to_base(fn_src, fn_dst, base=base):
     out_data = []
 
+    # represent a file as a big decimal number
     with open(fn_src, 'rb') as f:
         in_data = int.from_bytes(f.read(), 'big')
     
+    # convert a big decimal number to a baseN
     d, r = in_data % base, in_data // base
     out_data.append(abc[d])
     while r: 
         d, r = r % base, r // base
         out_data.append(abc[d])
 
+    # write a result as a string to a file
     with open(fn_dst, 'wb') as f:
         f.write(''.join(out_data).encode())
 
 def from_base(fn_src, fn_dst, base=base):
     out_data = 0
 
+    # read one long string at once to memory
     with open(fn_src, 'rb') as f:
         in_data = f.read().decode()
 
+    # convert a big baseN number to decimal
     for i, ch in enumerate(in_data):
         out_data = abc.index(ch)*(base**i) + out_data
 
+    # write a big decimal number to a file as a sequence of bytes
     with open(fn_dst, 'wb') as f:
         f.write(out_data.to_bytes(ceil(out_data.bit_length()/8), 'big'))
 
